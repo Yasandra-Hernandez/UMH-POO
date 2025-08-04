da1a841e3b8cc3e2fbb8906c18362c0e9d4077b7
family <family@FAMILY_INTSAM>
2025-08-04 15:19:08 -0600

    Agregar archivos de proyecto.

---
+++ proyecto de visualstudio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_de_visualstudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+       
+
+            Console.WriteLine("***CALCULO DEL SALARIO SEMANAL***");
+            
+            Console.WriteLine("Ingrese el costo por hora:");
+            double costoPorHora = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese las horas trabajadas:");
+            double horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+            double salarioSemanal = costoPorHora * horasTrabajadas;
+            Console.WriteLine($"Su salario semanal es de: {salarioSemanal}" );
+           
+           
+            Console.WriteLine("presione cualquier tecla`para salir");
+            Console.ReadLine();
+            
+        }
+    }
+}
